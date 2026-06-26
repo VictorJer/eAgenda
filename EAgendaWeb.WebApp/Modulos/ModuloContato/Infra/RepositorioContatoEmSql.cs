62e1a01c43c391d4fe856df14caa54ec4b232fa7
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-06-26 14:10:17 -0300

    ADD repositorioContatoEmSql finalizado

--- EAgendaWeb.WebApp/Modulos/ModuloContato/Infra/RepositorioContatoEmSql.cs
+++ EAgendaWeb.WebApp/Modulos/ModuloContato/Infra/RepositorioContatoEmSql.cs
@@ -1,10 +1,17 @@
+using Dapper;
 using EAgendaWeb.WebApp.Compartilhado.Infra.Sql;
+using Microsoft.Data.SqlClient;
 
 namespace EAgendaWeb.WebApp.Modulos.ModuloContato.Dominio;
 
 public sealed class RepositorioContatoEmSql(ISqlConnectionFactory ConnectionFactory)
     : IRepositorioContato
 {
+    private const string CadastrarSql = """
+    INSERT INTO [dbo].[TBContatos] (Id, Nome, Email, Telefone, Cargo, Empresa)
+    VALUES (@Id, @Nome, @Email, @Telefone, @Cargo, @Empresa);
+    """;
+
     private const string SelecionarTodosSql = """
     SELECT
         [Id],
@@ -17,7 +24,7 @@ public sealed class RepositorioContatoEmSql(ISqlConnectionFactory ConnectionFact
     ORDER BY [Nome];
     """;
 
-    private const string SelecionarPorId = """
+    private const string SelecionarPorIdSql = """
     SELECT
         [Id],
         [Nome],
@@ -29,33 +36,69 @@ public sealed class RepositorioContatoEmSql(ISqlConnectionFactory ConnectionFact
     WHERE [Id] = @Id;
     """;
 
+    private const string AtualizarSql = """
+    UPDATE [dbo].[TBCOntatos]
+    SET
+        [Nome] = @Nome,
+        [Email] = @Email,
+        [Telefone] = @Telefone,
+        [Cargo] = @Cargo,
+        [Empresa] = @Empresa
+    WHERE [Id] = @Id;
+    """;
+
+    private const string ExcluirSql = """
+    DELETE FROM [dbo].[TBContatos]
+    WHERE [Id] = @Id;
+    """;
+
     public void Cadastrar(Contato entidade)
     {
-        throw new NotImplementedException();
+        using SqlConnection conexao = ConnectionFactory.CreateConnection();
+
+        conexao.Open();
+
+        conexao.Execute(CadastrarSql, entidade);
     }
 
     public bool Editar(Guid idSelecionado, Contato entidadeAtualizada)
     {
-        throw new NotImplementedException();
+        using SqlConnection conexao = ConnectionFactory.CreateConnection();
+
+        conexao.Open();
+
+        return conexao.Execute(AtualizarSql, entidadeAtualizada) == 1;
     }
 
     public bool Excluir(Guid idSelecionado)
     {
-        throw new NotImplementedException();
-    }
+        using SqlConnection conexao = ConnectionFactory.CreateConnection();
 
-    public List<Contato> Filtrar(Predicate<Contato> filtro)
-    {
-        throw new NotImplementedException();
+        conexao.Open();
+
+        return conexao.Execute(ExcluirSql, new { Id = idSelecionado }) == 1;
     }
 
     public Contato? SelecionarPorId(Guid idSelecionado)
     {
-        throw new NotImplementedException();
+        using SqlConnection conexao = ConnectionFactory.CreateConnection();
+
+        conexao.Open();
+
+        return conexao.QuerySingleOrDefault<Contato>(SelecionarPorIdSql, new { Id = idSelecionado });
     }
 
     public List<Contato> SelecionarTodos()
     {
-        throw new NotImplementedException();
+        using SqlConnection conexao = ConnectionFactory.CreateConnection();
+
+        conexao.Open();
+
+        return conexao.Query<Contato>(SelecionarTodosSql).ToList();
+    }
+
+    public List<Contato> Filtrar(Predicate<Contato> filtro)
+    {
+        return SelecionarTodos().FindAll(filtro);
     }
 }
